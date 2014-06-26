f4f578730d119712dbf1e6a9763f1616663d9877
dkidney <darrenkidney@googlemail.com>
2014-06-26 15:37:16 +0100

    DK: minor change to prep4image

--- R/prep4image.r
+++ R/prep4image.r
@@ -13,8 +13,8 @@
 #'   \code{\link{image.plot}}
 #' @param contour if \code{TRUE} then contours will be added if (only used when 
 #'   \code{plot=TRUE})
-#' @param key logical for whether or not to plot color key with plot
-#' @param ... other arguments to pass to \code{\link{image.plot}} (only used 
+#' @param key logical for whether or not to include key when \code{plot = TRUE} (\code{\link{image.plot}} is used when \code{key = TRUE}, \code{\link{image}} is used when \code{key = FALSE})
+#' @param ... other arguments to pass to \code{\link{image}} or \code{\link{image.plot}} (only used 
 #'   when \code{plot=TRUE})
 #'   
 #' @details Sorts z on values of x first, then y, then creates a matrix of 
@@ -23,17 +23,36 @@
 #'   (matrix of z-values in appropriate order for image/contour/persp).
 #' @export
 #' @importFrom fields image.plot
+#' @examples
+#' data(Boland.alt)
+#' op = par(no.readonly = TRUE)
+#' par(mfrow = c(1,1))
+#' 
+#' # make object for use with image
+#' imagedata = prep4image(Boland.alt, plot = FALSE)
+#' image(imagedata, asp = 1)
+#' contour(imagedata, add = TRUE)
+#' 
+#' # use directly for plotting
+#' prep4image(Boland.alt, asp = 1)
+#' prep4image(Boland.alt, asp = 1, contour = FALSE)
+#' prep4image(Boland.alt, asp = 1, key = FALSE)
+#' prep4image(Boland.alt, asp = 1, contour = FALSE, key = FALSE)
+#' 
+#' par(op)
 
 prep4image = function(data, plot = TRUE, contour = TRUE, key = TRUE, ...){
   
-  x = sort(unique(data$x))
-  y = sort(unique(data$y))
+  data = as.matrix(data)
+  
+  x = sort(unique(data[,"x"]))
+  y = sort(unique(data[,"y"]))
   
   z = matrix(NA, nr = length(x), nc = length(y))
   for(i in 1:length(x)){
     for(j in 1:length(y)){
-      m = which(data$x == x[i] & data$y == y[j]) ; m
-      z[i,j] = if(length(m) == 0) NA else data$z[m]
+      m = which(data[,"x"] == x[i] & data[,"y"] == y[j]) ; m
+      z[i,j] = if(length(m) == 0) NA else data[,"z"][m]
     }
   }
   
