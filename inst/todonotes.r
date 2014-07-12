61bc6b01afd5ad8ea9647cb3ce79604f13d15e6e
david-borchers <dlb@Desktops-MacBook-Pro.local>
2014-07-12 17:30:26 +0100

    partial update of things to do

--- inst/todonotes.r
+++ inst/todonotes.r
@@ -1,13 +1,14 @@
 # 1. Add argument for creation of legend in image plot. Use image.plot() from library(fields) instead?
 # 2. Better plotting of factors in image.plot.mask
-# 3. Simulation function
-# 4. Density and abundance estimation function over given mask
-# 5. Why did AIC.secr not work?
-# 6. Update documentation, including:
-#    a. Remove references to B-splines and replace with any spline from mgcv.
+# 3. Update documentation, including:
 #    b. Make help for "secrgam" rather than (or as well as) "secrgam-package"
-#    c. Update Boland.mask documentation
-# 7. Convert these ot UTM: WCape.alt, WCape.alt.image, ...
-#
-#
+# 4. PlotDgam: 
+#    a. ?Fix CI bound plotting in plotDgam (missing beta_0?)
+#    b. Add option not to plot CI?
+#    c. (Maybe) rename "plotDgam" to "plotsmooth"?
+#    d. Bivariates smooths: plot marginals and 3D plot?
+#    e. Adapt plotDgam to plot 1D smooths when there are some multivariate smooth terms
+# 10. Add option to plot relative SE to plot.secrgam?
+# 11. Temporal smooths with Session.
+# 12. fitted.secrgam vs region.ND?
 
